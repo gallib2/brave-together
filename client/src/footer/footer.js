1348b796d0e6e035e8358863d825987b407a2c80
Yonatan Tin <yonitint1@gmail.com>
2021-09-10 20:50:48 +0300

    changed icons to a matching set

--- client/src/footer/footer.js
+++ client/src/footer/footer.js
@@ -12,21 +12,23 @@ import './footer.scss';
 const Footer = () => (
     <footer class="pv4 ph3 ph5-ns tc">
         <hr />
-        <a href="https://www.facebook.com/BraveTogether2018" title="facebook">
-            <FacebookIcon />
-        </a>
-        <a href='https://www.instagram.com/brave_together_insta/' title="instagram">
-            <InstagramIcon />
-        </a>
-        <a href="https://www.youtube.com/channel/UCHry9px4UrXGZWgptKYm0EQ" title="youtube">
-            <YoutubeIcon />
-        </a>
-        <a href="https://www.linkedin.com/company/brave-together-%D7%9E%D7%A6%D7%A2%D7%93-%D7%94%D7%92%D7%91%D7%95%D7%A8%D7%94/" title="linkdin">
-            <LinkdinIcon />    
-        </a>
-        <a href="https://github.com/gallib2/brave-together" title="github">
-            <GithubIcon />
-        </a>
+        <div className='social-icons-container'>
+            <a href="https://www.facebook.com/BraveTogether2018" title="facebook">
+                <FacebookIcon />
+            </a>
+            <a href='https://www.instagram.com/brave_together_insta/' title="instagram">
+                <InstagramIcon />
+            </a>
+            <a href="https://www.youtube.com/channel/UCHry9px4UrXGZWgptKYm0EQ" title="youtube">
+                <YoutubeIcon />
+            </a>
+            <a href="https://www.linkedin.com/company/brave-together-%D7%9E%D7%A6%D7%A2%D7%93-%D7%94%D7%92%D7%91%D7%95%D7%A8%D7%94/" title="linkdin">
+                <LinkdinIcon />    
+            </a>
+            <a href="https://github.com/gallib2/brave-together" title="github">
+                <GithubIcon />
+            </a>
+        </div>
         <div class="mt4">
             <a href="#" class="f6 link dim gray dib mr3 mr4-ns">עזרה</a>
             <a href="#" class="f6 link dim gray dib mr3 mr4-ns">צור קשר</a>
