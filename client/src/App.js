bdc2882ebb16d741d081368df5741be26f22ae76
Yonatan Tintpulver <yonitint1@gmail.com>
2021-09-24 00:23:32 +0300

    added token checks in storyCheck and testimony, added Homepage component.

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,21 @@ import TemplateEdit from './templateEdit/template-edit';
 import Share from './share/share';
 import About from './about/about';
 import Footer from './footer/footer'
+import Homepage from './homepage/homepage';
 
 //import BackButton from './backButton/backButton';
 import ThankYouForComing from './thankYouForComing/thankYouForComing';
 import Header from './header/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './auth/Login';
+import SignUp from './auth/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Header></Header>
       <Switch>
-        <Route exact path='/' component={Template} />
+        <Route exact path='/' component={Homepage} />
         <Route exact path='/storyCheck' component={StoryCheck} />
         <Route exact path='/testimony' component={Testimony} />
         <Route exact path='/textEdit' component={TextEdit} />
@@ -30,6 +33,8 @@ function App() {
         <Route exact path='/share' component={Share} />
         <Route exact path='/thankYouForComing' component={ThankYouForComing} />
         <Route exact path='/About' component={About} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={SignUp} />
         <Route path='*' component={() => '404 Not Found'} />
       </Switch>
       <Footer></Footer>
